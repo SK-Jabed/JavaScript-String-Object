a5cb013d8f38b72916ba2f5e00f6010357821012
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-02 22:17:54 +0600

    String JS File Done

--- string.js
+++ string.js
@@ -1,6 +1,28 @@
 const country = "Bangladesh";
 const division = 'Dhaka';
 const district = `Feni`;
-const thana = new String('Demra');
+// const thana = new String('Demra');
 
-console.log(thana);
+// console.log(thana);
+
+
+// String is immutable ==> not changeable;
+// Array is mutable ==> you can change the array elements
+
+const numbers = [20, 40, 60, 80, 100];
+console.log(numbers.length);
+console.log(numbers[1]);
+console.log(numbers);
+numbers[2] = 69;
+console.log(numbers);
+
+const capital = "Dhaka";
+console.log(capital.length);
+console.log(capital[2]);
+console.log(capital);
+capital[0] = "F";
+console.log(capital);
+
+const tea = "Cha Kha";
+console.log(tea.length);
+console.log(tea[3]);
