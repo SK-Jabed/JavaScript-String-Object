3b3ae4bc8947d8aa6a3f89dd10a9d567d3183c39
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-02 23:45:00 +0600

    Concat JS File Done and Dusted

--- concat.js
+++ concat.js
@@ -1,5 +1,17 @@
 const first = "Abid";
 const last = "Islam";
 
+// const fullName = first + last;
 const fullName = first + " " + last;
-console.log(fullName);  
+console.log(fullName); 
+const fullName2 = first.concat(last);
+console.log(fullName2);
+
+const fullName3= first.concat(" ").concat(last);
+console.log(fullName3);
+
+
+// includes
+
+console.log(last.includes("a"));
+console.log(last.includes("x"));
