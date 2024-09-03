53f043cea36e00cc3b563fa1d29e96109de18f14
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-03 12:32:36 +0600

    Object JS File Done and Dusted

---
+++ object.js
@@ -0,0 +1,35 @@
+// primitive
+// basic, primary
+
+const age = 18;
+const institute = "Dhaka Polytechnic Institute";
+const isPassed = true;
+// let isDeveloper;
+
+const subjects = ["Bangla", "English", "Physics", "Math"];
+const bottle_wrong = ["white", 45];
+const bottle_color = "white";
+const bottlePrice = 45;
+const bottleDrinks = "water";
+
+// object 
+// non-primitive
+
+const bottle = {
+  brand: "apple", 
+  price: 45,
+  color: "white",
+  isClean: true,
+  drinks: "water"
+}
+
+const subject = {
+    name: "Biology",
+    teacher: "Fahad sir",
+    examDate: "22 Sep",
+    chapters: ["first", "second", "third"],
+    exams: {
+      name: "Final Exam",
+      marks: 100
+    }
+}
