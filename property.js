5ff5a3c9b57de2459449bb34a0c0c374563dd081
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-03 12:54:12 +0600

    Property JS File Done and Dusted

---
+++ property.js
@@ -0,0 +1,27 @@
+const person = {
+  name: "Habu Mia",
+  age: 42,
+  profession: "Developer",
+  salary: 25000,
+  married: true,
+  "fav places": ["Bandorbon", "Saint-Martin", "Cox's Bazar"] 
+}
+
+// console.log(person);
+
+// dot notation
+// Access property value of object by using dot symbol
+
+console.log(person.profession);
+const income = person.salary;
+console.log(income);
+
+// bracket notation
+// Access with third bracket
+console.log(person["age"]);
+
+const boyos = person["age"];
+console.log(boyos);
+
+// console.log(person."fav places");
+console.log(person["fav places"]);
